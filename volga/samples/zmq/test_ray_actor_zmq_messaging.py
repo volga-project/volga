f2629c0dd074ba5a567b3b981ca45e5a5e69a89b
dirtyValera <valera.dirty@gmail.com>
2024-05-24 12:01:32 +0400

    Test local ZeroMQ push-pull backpressure WIP

--- volga/samples/zmq/test_ray_actor_zmq_messaging.py
+++ volga/samples/zmq/test_ray_actor_zmq_messaging.py
@@ -1,15 +1,39 @@
 import time
-import random
-from socket import socket
+from typing import Dict, Tuple, List
+
+import simplejson
 
 import ray
 import zmq
 
 # HWM
 # https://stackoverflow.com/questions/53356451/pyzmq-high-water-mark-not-working-on-pub-socket
+# https://stackoverflow.com/questions/22613737/how-could-i-set-hwm-in-the-push-pull-pattern-of-zmq/26424611#26424611
 
 # EAGAIN
 # https://github.com/zeromq/libzmq/issues/1332
+
+
+class Message:
+    def __init__(self, msg_id: int, payload: Dict):
+        self.msg_id = msg_id
+        self.payload = payload
+
+    def ser(self) -> str:
+        return simplejson.dumps({
+            'msg_id': self.msg_id,
+            'payload': self.payload
+        })
+
+    @staticmethod
+    def de(json_str: str) -> 'Message':
+        d = simplejson.loads(json_str)
+        return Message(msg_id=d['msg_id'], payload=d['payload'])
+
+
+def _msg(msg_id: int) -> Message:
+    payload = {'value': f'msg_{msg_id}', 'dummy_heavy_load': 'a'*10000}
+    return Message(msg_id=msg_id, payload=payload)
 
 
 @ray.remote
@@ -18,26 +42,48 @@ class Push:
     def __init__(self):
         self.port = None
         self.socket = None
+        self.total_time = None
+        self.backpressure_intervals = []
 
-    def initialize(self, port: int):
+    def initialize(self, port: int, snd_hwm: int, rcv_hwm: int):
         self.port = port
         context = zmq.Context()
         self.socket = context.socket(zmq.PUSH)
-        self.socket.bind(f'tcp://127.0.0.1:{port}')
-        pass
+        self.socket.setsockopt(zmq.SNDHWM, snd_hwm)
+        self.socket.setsockopt(zmq.RCVHWM, rcv_hwm)
+        # self.socket
+        self.socket.bind(f'tcp://*:{port}')
+        print('Push inited')
 
-    def start(self):
-        num_events = 1000000
+    def start(self, num_events: int, loop_timeout_s: float):
+        start = time.time()
         for i in range(num_events):
-            # topic = random.randrange(10000, 10002)
-            # messagedata = random.randrange(1, 215) - 80
-            # self.socket.send_string("%d %d" % (topic, messagedata))
-            msg = f'msg_{i}'
-            self.socket.send_string(msg)
-            # time.sleep(0.0000001)
-            # print(f'sent {msg}')
+            msg = _msg(i)
+            send_start = time.time()
+            is_backpressured = False
+            while True:
+                try:
+                    self.socket.send_string(msg.ser(), zmq.NOBLOCK)
+                    print(f'sent {msg.msg_id}')
+                    if is_backpressured:
+                        is_backpressured = False
+                        self.backpressure_intervals.append((send_start, time.time()))
+                        print(f'Backpressure finished after {time.time() - send_start}s')
+                    break
+                except zmq.error.Again:
+                    if not is_backpressured:
+                        print('Backpressure started')
+                    is_backpressured = True
+                    pass
+                time.sleep(0.01)
+            time.sleep(loop_timeout_s)
+        self.total_time = time.time() - start
         time.sleep(1)
         self.socket.send_string('done')
+
+    def get_backpressure_stats(self) -> Tuple[List, float]:
+        return self.backpressure_intervals, self.total_time
+
 
 @ray.remote
 class Pull:
@@ -45,39 +91,64 @@ class Pull:
         self.port = None
         self.socket = None
 
-    def initialize(self, producer_addr: str):
+    def initialize(self, peer_addr: str, snd_hwm: int, rcv_hwm: int):
         context = zmq.Context()
         self.socket = context.socket(zmq.PULL)
-        self.socket.connect(producer_addr)
+        self.socket.setsockopt(zmq.SNDHWM, snd_hwm)
+        self.socket.setsockopt(zmq.RCVHWM, rcv_hwm)
+        self.socket.connect(peer_addr)
+        print('Pull inited')
 
-    def start(self):
-        # topicfilter = '10001'
-        # self.socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
-        # self.socket.subscribe('') # subscribe to all topics
-        # Process 5 updates
-        received = 0
+    def start(self, start_delay_s: float, loop_timeout_s: float) -> int:
+        num_received = 0
+        time.sleep(start_delay_s)
         while True:
-            # string = self.socket.recv()
-            # topic, messagedata = string.split()
             s = self.socket.recv_string()
-            # print(s)
             if s == 'done':
                 break
-            received += 1
+            msg = Message.de(s)
+            print(f'rcvd {msg.msg_id}')
+            num_received += 1
+            time.sleep(loop_timeout_s)
 
         time.sleep(1)
-
-        print(f'received {received}')
+        print(f'num received: {num_received}')
         print('Done')
         time.sleep(1)
+        return num_received
 
 
 with ray.init(address='auto'):
-    pub_port = 5151
-    pub_addr = f'tcp://127.0.0.1:{pub_port}'
+
+    # sender config
+    num_events = 300
+    push_loop_timeout_s = 0.01
+    push_snd_hwm = 100
+    push_rcv_hwm = 0
+
+    # receiver config
+    pull_start_delay_s = 0
+    pull_loop_timeout_s = 0.02
+    pull_snd_hwm = 1
+    pull_rcv_hwm = 1
+
+    peer_port = 5152
+    peer_addr = f'tcp://127.0.0.1:{peer_port}'
     push = Push.remote()
-    ray.get(push.initialize.remote(pub_port))
+    ray.get(push.initialize.remote(peer_port, push_snd_hwm, push_rcv_hwm))
     pull = Pull.remote()
-    ray.get(pull.initialize.remote(pub_addr))
-    push.start.remote()
-    ray.get(pull.start.remote())
+    ray.get(pull.initialize.remote(peer_addr, pull_snd_hwm, pull_rcv_hwm))
+    push.start.remote(num_events, push_loop_timeout_s)
+    num_received = ray.get(pull.start.remote(pull_start_delay_s, pull_loop_timeout_s))
+
+    # calculate backpressure stats
+    backpressure_intervals, push_total_time = ray.get(push.get_backpressure_stats.remote())
+    backpressure_durations = list(map(lambda e: e[1] - e[0], backpressure_intervals))
+    total_backpressured_time = sum(backpressure_durations)
+    backpressure = total_backpressured_time / push_total_time
+    bprs_stats = f'{len(backpressure_intervals)} backpressure intervals: {backpressure_durations} \n' \
+           f'Backpressure {backpressure} \n' \
+           f'Total push time: {push_total_time}'
+    print(bprs_stats)
+
+    assert num_events == num_received
