0e7944784544a74c700678e0749c1f152b97ab8e
dirtyValera <valera.dirty@gmail.com>
2024-07-28 11:50:35 +0400

    [Rustify Network] Simple perf test

--- tests/test_rust_binding.py
+++ tests/test_rust_binding.py
@@ -1,6 +1,7 @@
 from volga_rust import Channel, LocalChannel, RemoteChannel, RustDataReader, RustDataWriter, RustIOLoop
 import msgpack
 import time
+import threading
 
 lc = LocalChannel('ch_0', 'ipc:///tmp/ipc_0')
 
@@ -11,15 +12,34 @@ io_loop.register_data_writer(data_writer)
 io_loop.register_data_reader(data_reader)
 data_reader.start()
 io_loop.start(1)
-msg = 'abc'
-b = msgpack.dumps(msg)
-data_writer.write_bytes(lc.channel_id, b, 1000, 1)
 
-time.sleep(1)
+num_msgs = 100000
+msg_size = 32*1024
 
-_b = data_reader.read_bytes()
-_msg = msgpack.loads(_b)
+msgs = ['a' * msg_size] * num_msgs
+
+def send():
+    for msg in msgs:
+        b = msgpack.dumps(msg)
+        res = None
+        while res is None:
+            res = data_writer.write_bytes(lc.channel_id, b, 1000, 1)
+
+wt = threading.Thread(target=send)
+
+start_ts = time.time()
+wt.start()
+
+rcvd = []
+
+while len(rcvd) != num_msgs:
+    _b = None
+    while _b is None:
+        _b = data_reader.read_bytes()
+    _msg = msgpack.loads(_b)
+    rcvd.append(_msg)
+run_ts = time.time() - start_ts
 io_loop.close()
 data_reader.close()
-assert msg == _msg
-print('Assert ok')
+assert msgs == rcvd
+print(f'Assert ok, finished in {run_ts}s')
